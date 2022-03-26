b427139abf3d2a30cb42e9a1a27bfb9e9107d3ca
KhaledBadran <badrankhaled97@gmail.com>
2022-03-26 17:58:42 -0400

    Add LFR + OptimalPreproc with exception handeling

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -2,7 +2,7 @@
 # https://github.com/Trusted-AI/AIF360/blob/master/examples/tutorial_medical_expenditure.ipynb
 # https://github.com/Trusted-AI/AIF360/blob/master/examples/demo_meta_classifier.ipynb
 # https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
-
+import copy
 import sys
 
 from sklearn.metrics import accuracy_score
@@ -24,6 +24,7 @@ from aif360.sklearn.datasets import fetch_german
 from aif360.metrics import BinaryLabelDatasetMetric
 from aif360.metrics import ClassificationMetric
 from aif360.datasets import BinaryLabelDataset
+from aif360.algorithms.preprocessing.optim_preproc_helpers.opt_tools import OptTools
 
 # Explainers
 from aif360.explainers import MetricTextExplainer
@@ -64,30 +65,41 @@ def train_test_models(train_dataset, test_dataset, dataset_info):
 
     for clf_name, clf in CLASSIFIERS.items():
         print(f"\nevaluating classifier {clf_name}")
-        clf.fit(X_train, y_train, sample_weight=train_dataset.instance_weights)
 
-        y_pred = clf.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        print(f"accuracy {accuracy}")
+        # Check if the training labels are not all the same
+        if len(set(y_train)) != 1:
+            clf.fit(X_train, y_train, sample_weight=train_dataset.instance_weights)
 
-        classified_dataset = test_dataset.copy()
-        classified_dataset.labels = y_pred
-        classification_metric = ClassificationMetric(
-            dataset=test_dataset,
-            classified_dataset=classified_dataset,
-            unprivileged_groups=dataset_info["unprivileged_groups"],
-            privileged_groups=dataset_info["privileged_groups"],
-        )
-        print(f"disparate impact {classification_metric.disparate_impact()}")
-        print(
-            f"average odds difference {classification_metric.average_odds_difference()}"
-        )
+            y_pred = clf.predict(X_test)
 
-        results[clf_name] = {
-            "accuracy": accuracy,
-            "disparate_impact": classification_metric.disparate_impact(),
-            "average_odds_difference": classification_metric.average_odds_difference(),
-        }
+
+            classified_dataset = test_dataset.copy()
+            classified_dataset.labels = y_pred
+            classification_metric = ClassificationMetric(
+                dataset=test_dataset,
+                classified_dataset=classified_dataset,
+                unprivileged_groups=dataset_info["unprivileged_groups"],
+                privileged_groups=dataset_info["privileged_groups"],
+            )
+
+            # calculate metrics
+            accuracy = accuracy_score(y_test, y_pred)
+            disparate_impact = classification_metric.disparate_impact()
+            average_odds_difference = classification_metric.average_odds_difference()
+
+            print(f"accuracy {accuracy}")
+            print(f"disparate_impact {disparate_impact}")
+            print(f"average odds difference {average_odds_difference}")
+
+            results[clf_name] = {
+                "accuracy": accuracy,
+                "disparate_impact": disparate_impact,
+                "average_odds_difference": average_odds_difference,
+            }
+
+        else:
+            print(f'all training labels are same, classifier will not be trained')
+            results[clf_name] = {}
 
     return dict(results)
 
@@ -114,33 +126,65 @@ def initial_preprocessing(train_dataset: BinaryLabelDataset, test_dataset):
 
 def apply_preprocessing_algo(
     algo_name,
-    transformer,
+    algo_transformer,
     train_dataset: BinaryLabelDataset,
     test_dataset: BinaryLabelDataset,
     dataset_info,
 ):
 
-    if algo_name == "Reweighing":
-        # RW = Reweighing(privileged_groups=dataset_info['privileged_groups'],
-        #                 unprivileged_groups=dataset_info['unprivileged_groups'])
-        train_dataset_transformed = transformer.fit_transform(train_dataset)
-        test_dataset_transformed = test_dataset.copy()
+    # Copy the transformer to avoid bugs
+    transformer = copy.deepcopy(algo_transformer)
 
-    elif algo_name == "DisparateImpactRemover":
-        # DIR = DisparateImpactRemover(sensitive_attribute=dataset_info['sensitive_attribute'])
+    # Initialize the transformed datasets
+    train_dataset_transformed = train_dataset.copy(deepcopy=True)
+    test_dataset_transformed = test_dataset.copy(deepcopy=True)
 
-        index = train_dataset.feature_names.index(dataset_info["sensitive_attribute"])
+    try:
+        if algo_name == "Reweighing":
+            # RW = Reweighing(privileged_groups=dataset_info['privileged_groups'],
+            #                 unprivileged_groups=dataset_info['unprivileged_groups'])
+            train_dataset_transformed = transformer.fit_transform(train_dataset)
+            test_dataset_transformed = test_dataset.copy(deepcopy=True)
 
-        train_dataset_transformed = transformer.fit_transform(train_dataset)
-        test_dataset_transformed = transformer.fit_transform(test_dataset)
+        elif algo_name == "DisparateImpactRemover":
+            # DIR = DisparateImpactRemover(sensitive_attribute=dataset_info['sensitive_attribute'])
 
-        # delete protected columns
-        train_dataset_transformed.features = np.delete(
-            train_dataset_transformed.features, index, axis=1
-        )
-        test_dataset_transformed.features = np.delete(
-            test_dataset_transformed.features, index, axis=1
-        )
+            index = train_dataset.feature_names.index(dataset_info["sensitive_attribute"])
+
+            train_dataset_transformed = transformer.fit_transform(train_dataset)
+            test_dataset_transformed = transformer.fit_transform(test_dataset)
+
+            # delete protected columns
+            train_dataset_transformed.features = np.delete(
+                train_dataset_transformed.features, index, axis=1
+            )
+            test_dataset_transformed.features = np.delete(
+                test_dataset_transformed.features, index, axis=1
+            )
+
+        elif algo_name == "OptimPreproc":
+
+            transformer = transformer.fit(train_dataset)
+            train_dataset_transformed = transformer.transform(train_dataset, transform_Y=True)
+
+            train_dataset_transformed = train_dataset.align_datasets(train_dataset_transformed)
+
+            test_dataset_transformed = transformer.transform(test_dataset, transform_Y=True)
+            test_dataset_transformed = test_dataset.align_datasets(test_dataset_transformed)
+
+        elif algo_name == "LFR":
+
+            transformer = transformer.fit(train_dataset, maxiter=5000, maxfun=5000)
+
+            # Transform training data and align features
+            train_dataset_transformed = transformer.transform(train_dataset, threshold=0.35)
+            test_dataset_transformed = transformer.transform(test_dataset, threshold=0.35)
+
+
+
+    except Exception as e:
+        print(f'Failed to pre-process the dataset. The error msg is:')
+        print(e)
 
     return (
         train_dataset_transformed,
@@ -157,13 +201,18 @@ def initialize_debaiasing_algorithms(dataset_info):
         "DisparateImpactRemover": DisparateImpactRemover(
             sensitive_attribute=dataset_info["sensitive_attribute"]
         ),
-        # "OptimPreproc": OptimPreproc(
-        #     OptTools,
-        #     dataset_info["optim_options"],
-        #     unprivileged_groups=dataset_info["unprivileged_groups"],
-        #     privileged_groups=dataset_info["privileged_groups"],
-        # )
-        # 'LFR': LFR(privileged_groups=privileged_groups, unprivileged_groups=unprivileged_groups)
+        "OptimPreproc": OptimPreproc(
+            OptTools,
+            dataset_info["optim_options"],
+            verbose=False,
+            # This algorithm does not use the privileged and unprivileged groups specified during initialization
+            # unprivileged_groups=dataset_info["unprivileged_groups"],
+            # privileged_groups=dataset_info["privileged_groups"],
+        ),
+        'LFR': LFR(unprivileged_groups=dataset_info['unprivileged_groups'],
+                 privileged_groups=dataset_info['privileged_groups'],
+                 k=5, Ax=0.01, Ay=1.0, Az=50.0, verbose=0    # Default parameters
+                )
     }
 
 
@@ -181,20 +230,20 @@ def main():
             dataset_info=dataset_info
         )
 
-        train_split, test_split = dataset.split([0.7], shuffle=True)
-        train_split, test_split = initial_preprocessing(train_split, test_split)
+        train_split, test_split = dataset.split([0.7], shuffle=True, seed=0)
+        # train_split, test_split = initial_preprocessing(train_split, test_split)
         results[dataset_name]["baseline"] = train_test_models(
             train_split, test_split, dataset_info=dataset_info
         )
 
-        for (debaiasing_algo_name,debaiasing_transformer,) in debaiasing_algorithms.items():
+        for (debaiasing_algo_name, debaiasing_transformer,) in debaiasing_algorithms.items():
             print(f"\n\n####After applying {debaiasing_algo_name}######\n")
 
             train_split_transformed, test_split_transformed = apply_preprocessing_algo(
                 debaiasing_algo_name,
                 debaiasing_transformer,
-                train_split,
-                test_split,
+                train_split.copy(deepcopy=True),
+                test_split.copy(deepcopy=True),
                 dataset_info,
             )
 
@@ -204,8 +253,9 @@ def main():
                 dataset_info=dataset_info,
             )
 
-    print(json.dumps(results, indent=4))
-
+    # print(json.dumps(results, indent=4))
+    with open('results.json', 'w') as fp:
+        json.dump(results, fp, indent=4)
 
 if __name__ == "__main__":
     main()
