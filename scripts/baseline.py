c0d428a6f8dfe9de1fcf1a9946cc4ce427afc930
KhaledBadran <badrankhaled97@gmail.com>
2022-03-26 19:50:38 -0400

    Support hyperparmeters for preprocessing algorthims

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -2,43 +2,22 @@
 # https://github.com/Trusted-AI/AIF360/blob/master/examples/tutorial_medical_expenditure.ipynb
 # https://github.com/Trusted-AI/AIF360/blob/master/examples/demo_meta_classifier.ipynb
 # https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
-import copy
-import sys
-
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import RepeatedStratifiedKFold, train_test_split
-from sklearn.model_selection import GridSearchCV
-
-import matplotlib.pyplot as plt
 import numpy as np
-from IPython.display import Markdown, display
 import pandas as pd
-
-# Datasets
-from aif360.datasets import AdultDataset
-from aif360.datasets import CompasDataset
-from aif360.datasets import GermanDataset
-from aif360.sklearn.datasets import fetch_german
+import json
+from collections import defaultdict
 
 # Fairness metrics
 from aif360.metrics import BinaryLabelDatasetMetric
 from aif360.metrics import ClassificationMetric
 from aif360.datasets import BinaryLabelDataset
-from aif360.algorithms.preprocessing.optim_preproc_helpers.opt_tools import OptTools
+from sklearn.metrics import accuracy_score
 
 # Explainers
 from aif360.explainers import MetricTextExplainer
 
 # Scalers
 from sklearn.preprocessing import StandardScaler
-
-# Classifiers
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
-from aif360.sklearn.metrics import disparate_impact_ratio, average_odds_error
-import pandas as pd
-import json
 
 # Bias mitigation techniques
 from aif360.algorithms.preprocessing import (
@@ -49,14 +28,32 @@ from aif360.algorithms.preprocessing import (
 )
 
 from aif360.algorithms.preprocessing.optim_preproc_helpers.opt_tools import OptTools
+
+# Experiment constants
 from constants import DATASETS, CLASSIFIERS, HYPERPARAMETERS
 
-from collections import defaultdict
+# typechecking
+from typeguard import typechecked
+from typing import Dict, List, Tuple, Union
 
 np.random.seed(0)
 
 
-def train_test_models(train_dataset, test_dataset, dataset_info):
+@typechecked
+def train_test_models(
+    train_dataset: BinaryLabelDataset,
+    test_dataset: BinaryLabelDataset,
+    dataset_info: Dict,
+) -> Dict:
+    """
+    returns the accuracy and fairness metrics based after training and testing a model.
+
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :return: a dictionary of accuracy and fairness metrics (e.g., disparate_impact) with the values calculated using
+    a trained model
+    """
     results = defaultdict(dict)
 
     # Train models
@@ -71,7 +68,6 @@ def train_test_models(train_dataset, test_dataset, dataset_info):
             clf.fit(X_train, y_train, sample_weight=train_dataset.instance_weights)
 
             y_pred = clf.predict(X_test)
-
 
             classified_dataset = test_dataset.copy()
             classified_dataset.labels = y_pred
@@ -98,13 +94,22 @@ def train_test_models(train_dataset, test_dataset, dataset_info):
             }
 
         else:
-            print(f'all training labels are same, classifier will not be trained')
+            print(f"all training labels are same, classifier will not be trained")
             results[clf_name] = {}
 
     return dict(results)
 
-# @typehceck
-def initial_preprocessing(train_dataset: BinaryLabelDataset, test_dataset):
+
+@typechecked
+def initial_preprocessing(
+    train_dataset: BinaryLabelDataset, test_dataset: BinaryLabelDataset
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    Applies scaling to the dataset
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :return: a scaled training and test AIF360 datasets
+    """
     # get X and y for training and test splits
     X_train, y_train = train_dataset.features, train_dataset.labels.ravel()
     X_test, y_test = test_dataset.features, test_dataset.labels.ravel()
@@ -124,78 +129,159 @@ def initial_preprocessing(train_dataset: BinaryLabelDataset, test_dataset):
     return train_dataset_preprocessed, test_dataset_preprocessed
 
 
-def apply_preprocessing_algo(
-    algo_name,
-    # algo_transformer,
+@typechecked
+def apply_reweighing(
     train_dataset: BinaryLabelDataset,
     test_dataset: BinaryLabelDataset,
-    dataset_info,
-):
+    dataset_info: Dict,
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :return: a train and test datasets that have been transformed via reweighing
+    """
+    RW = Reweighing(
+        privileged_groups=dataset_info["privileged_groups"],
+        unprivileged_groups=dataset_info["unprivileged_groups"],
+    )
+    train_dataset_RW = RW.fit_transform(train_dataset)
+    test_dataset_RW = test_dataset.copy(deepcopy=True)
 
-    # Copy the transformer to avoid bugs
-    # transformer = copy.deepcopy(algo_transformer)
+    return train_dataset_RW, test_dataset_RW
 
-    # Initialize the transformed datasets
-    train_dataset_transformed = train_dataset.copy(deepcopy=True)
-    test_dataset_transformed = test_dataset.copy(deepcopy=True)
 
+def apply_DIR(
+    train_dataset: BinaryLabelDataset,
+    test_dataset: BinaryLabelDataset,
+    dataset_info: Dict,
+    hyperparameters: Dict,
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :param hyperparameters: a dictionary containing the value of the repair_level parameter
+    :return: a train and test datasets that have been transformed via Disparate Impact Remover technique
+    """
+    DIR = DisparateImpactRemover(
+        sensitive_attribute=dataset_info["sensitive_attribute"],
+        repair_level=hyperparameters["repair_level"],
+    )
+    index = train_dataset.feature_names.index(dataset_info["sensitive_attribute"])
+
+    train_dataset_DIR = DIR.fit_transform(train_dataset)
+    test_dataset_DIR = DIR.fit_transform(test_dataset)
+
+    # delete protected columns
+    train_dataset_DIR.features = np.delete(train_dataset_DIR.features, index, axis=1)
+    test_dataset_DIR.features = np.delete(test_dataset_DIR.features, index, axis=1)
+
+    return train_dataset_DIR, test_dataset_DIR
+
+
+@typechecked()
+def apply_OptimPreproc(
+    train_dataset: BinaryLabelDataset,
+    test_dataset: BinaryLabelDataset,
+    dataset_info: Dict,
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :return: a train and test datasets that have been transformed via the Optimized Preprocessing technique
+    """
+
+    OP = OptimPreproc(OptTools, dataset_info["optim_options"], verbose=False)
+
+    OP = OP.fit(train_dataset)
+
+    train_dataset_OP = OP.transform(train_dataset, transform_Y=True)
+    train_dataset_OP = train_dataset.align_datasets(train_dataset_OP)
+
+    test_dataset_OP = OP.transform(test_dataset, transform_Y=True)
+    test_dataset_OP = test_dataset.align_datasets(test_dataset_OP)
+
+    return train_dataset_OP, test_dataset_OP
+
+
+@typechecked
+def apply_LFR(
+    train_dataset: BinaryLabelDataset,
+    test_dataset: BinaryLabelDataset,
+    dataset_info: Dict,
+    hyperparameters: Dict,
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :param hyperparameters: a dictionary containing both the hyperparameters for the LFR object (e.g., Ax) and the
+    threshold for the transform method
+    :return: a train and test datasets that have been transformed via the LFR technique
+    """
+    LFR_transformer = LFR(
+        unprivileged_groups=dataset_info["unprivileged_groups"],
+        privileged_groups=dataset_info["privileged_groups"],
+        k=hyperparameters["k"],
+        Ax=hyperparameters["Ax"],
+        Ay=hyperparameters["Ay"],
+        Az=hyperparameters["Az"],
+        verbose=0,  # Default parameters
+    )
+
+    LFR_transformer = LFR_transformer.fit(train_dataset, maxiter=5000, maxfun=5000)
+
+    # Transform training data and align features
+    train_dataset_LFR = LFR_transformer.transform(
+        train_dataset, threshold=hyperparameters["threshold"]
+    )
+    test_dataset_LFR = LFR_transformer.transform(
+        test_dataset, threshold=hyperparameters["threshold"]
+    )
+
+    return train_dataset_LFR, test_dataset_LFR
+
+
+@typechecked
+def apply_preprocessing_algo(
+    algo_name: str,
+    hyperparameters: Dict,
+    train_dataset: BinaryLabelDataset,
+    test_dataset: BinaryLabelDataset,
+    dataset_info: Dict,
+) -> Tuple[BinaryLabelDataset, BinaryLabelDataset]:
+    """
+    :param algo_name: name of the preprocessing technique to apply
+    :param hyperparameters: used to tune the preprocessing technique (can be an empty dict)
+    :param train_dataset: an AIF360 dataset containing the training examples with their labels
+    :param test_dataset: an AIF360 dataset containing the test examples with their labels
+    :param dataset_info: information about the dataset including privileged and unprivileged groups
+    :return: a train and test datasets that have been transformed via one of the preprocessing techniques
+    """
     try:
         if algo_name == "Reweighing":
-            RW = Reweighing(
-                privileged_groups=dataset_info["privileged_groups"],
-                unprivileged_groups=dataset_info["unprivileged_groups"],
+            train_dataset_transformed, test_dataset_transformed = apply_reweighing(
+                train_dataset, test_dataset, dataset_info
             )
-            train_dataset_transformed = RW.fit_transform(train_dataset)
-            test_dataset_transformed = test_dataset.copy(deepcopy=True)
-
         elif algo_name == "DisparateImpactRemover":
-            DIR = DisparateImpactRemover(
-                sensitive_attribute=dataset_info["sensitive_attribute"]
-            )
-            index = train_dataset.feature_names.index(dataset_info["sensitive_attribute"])
-
-            train_dataset_transformed = DIR.fit_transform(train_dataset)
-            test_dataset_transformed = DIR.fit_transform(test_dataset)
-
-            # delete protected columns
-            train_dataset_transformed.features = np.delete(
-                train_dataset_transformed.features, index, axis=1
-            )
-            test_dataset_transformed.features = np.delete(
-                test_dataset_transformed.features, index, axis=1
+            train_dataset_transformed, test_dataset_transformed = apply_DIR(
+                train_dataset, test_dataset, dataset_info, hyperparameters
             )
 
         elif algo_name == "OptimPreproc":
-
-            OP = OptimPreproc(
-                OptTools,
-                dataset_info["optim_options"],
-                verbose=False)
-
-            OP = OP.fit(train_dataset)
-            train_dataset_transformed = OP.transform(train_dataset, transform_Y=True)
-
-            train_dataset_transformed = train_dataset.align_datasets(train_dataset_transformed)
-
-            test_dataset_transformed = OP.transform(test_dataset, transform_Y=True)
-            test_dataset_transformed = test_dataset.align_datasets(test_dataset_transformed)
+            train_dataset_transformed, test_dataset_transformed = apply_OptimPreproc(
+                train_dataset, test_dataset, dataset_info
+            )
 
         elif algo_name == "LFR":
-
-            LFR_transformer = LFR(unprivileged_groups=dataset_info['unprivileged_groups'],
-                privileged_groups=dataset_info['privileged_groups'],
-                k=5, Ax=0.01, Ay=1.0, Az=50.0, verbose=0  # Default parameters
-                )
-
-            LFR_transformer = LFR_transformer.fit(train_dataset, maxiter=5000, maxfun=5000)
-
-            # Transform training data and align features
-            train_dataset_transformed = LFR_transformer.transform(train_dataset, threshold=0.35)
-            test_dataset_transformed = LFR_transformer.transform(test_dataset, threshold=0.35)
-
+            train_dataset_transformed, test_dataset_transformed = apply_LFR(
+                train_dataset, test_dataset, dataset_info, hyperparameters
+            )
 
     except Exception as e:
-        print(f'Failed to pre-process the dataset. The error msg is:')
+        print(f"Failed to pre-process the dataset. The error msg is:")
         print(e)
 
     return (
@@ -204,43 +290,36 @@ def apply_preprocessing_algo(
     )
 
 
-def initialize_debaiasing_algorithms(dataset_info):
-    return {
-        "Reweighing": Reweighing(
-            privileged_groups=dataset_info["privileged_groups"],
-            unprivileged_groups=dataset_info["unprivileged_groups"],
-        ),
-        "DisparateImpactRemover": DisparateImpactRemover(
-            sensitive_attribute=dataset_info["sensitive_attribute"]
-        ),
-        "OptimPreproc": OptimPreproc(
-            OptTools,
-            dataset_info["optim_options"],
-            verbose=False,
-            # This algorithm does not use the privileged and unprivileged groups specified during initialization
-            # unprivileged_groups=dataset_info["unprivileged_groups"],
-            # privileged_groups=dataset_info["privileged_groups"],
-        ),
-        'LFR': LFR(unprivileged_groups=dataset_info['unprivileged_groups'],
-                 privileged_groups=dataset_info['privileged_groups'],
-                 k=5, Ax=0.01, Ay=1.0, Az=50.0, verbose=0    # Default parameters
-                )
-    }
+# @typechecked()
+# def initialize_debaiasing_algorithms(dataset_info: Dict):
+#     return {
+#         "Reweighing": Reweighing(
+#             privileged_groups=dataset_info["privileged_groups"],
+#             unprivileged_groups=dataset_info["unprivileged_groups"],
+#         ),
+#         "DisparateImpactRemover": DisparateImpactRemover(
+#             sensitive_attribute=dataset_info["sensitive_attribute"]
+#         ),
+#         "OptimPreproc": OptimPreproc(
+#             OptTools,
+#             dataset_info["optim_options"],
+#             verbose=False,
+#         ),
+#         'LFR': LFR(unprivileged_groups=dataset_info['unprivileged_groups'],
+#                    privileged_groups=dataset_info['privileged_groups'],
+#                    k=5, Ax=0.01, Ay=1.0, Az=50.0, verbose=0  # Default parameters
+#                    )
+#     }
 
 
 def main():
-
     results = defaultdict(dict)
 
     for dataset_name, dataset_info in DATASETS.items():
 
         print(f"\n\n$$$$in dataset {dataset_name}$$$$$\n")
 
-        dataset = dataset_info["original_dataset"]
-
-        # debaiasing_algorithms = initialize_debaiasing_algorithms(
-        #     dataset_info=dataset_info
-        # )
+        dataset: BinaryLabelDataset = dataset_info["original_dataset"]
 
         train_split, test_split = dataset.split([0.7], shuffle=True, seed=0)
         # train_split, test_split = initial_preprocessing(train_split, test_split)
@@ -248,25 +327,40 @@ def main():
             train_split, test_split, dataset_info=dataset_info
         )
 
-        for (debaiasing_algo_name, hyperparameters,) in HYPERPARAMETERS.items():
+        for (
+            debaiasing_algo_name,
+            hyperparameters_space,
+        ) in HYPERPARAMETERS.items():
             print(f"\n\n####After applying {debaiasing_algo_name}######\n")
 
-            train_split_transformed, test_split_transformed = apply_preprocessing_algo(
-                debaiasing_algo_name,
-                train_split.copy(deepcopy=True),
-                test_split.copy(deepcopy=True),
-                dataset_info,
-            )
+            results[dataset_name][debaiasing_algo_name] = []
 
-            results[dataset_name][debaiasing_algo_name] = train_test_models(
-                train_split_transformed,
-                test_split_transformed,
-                dataset_info=dataset_info,
-            )
+            for hyperparameters in hyperparameters_space:
+                (
+                    train_split_transformed,
+                    test_split_transformed,
+                ) = apply_preprocessing_algo(
+                    debaiasing_algo_name,
+                    hyperparameters,
+                    train_split.copy(deepcopy=True),
+                    test_split.copy(deepcopy=True),
+                    dataset_info,
+                )
 
-    # print(json.dumps(results, indent=4))
-    with open('results_2.json', 'w') as fp:
+                performance_metrics = train_test_models(
+                    train_split_transformed,
+                    test_split_transformed,
+                    dataset_info=dataset_info,
+                )
+
+                # record the used hyperparameters
+                results[dataset_name][debaiasing_algo_name].append(
+                    {"hyperparameters": hyperparameters, "results": performance_metrics}
+                )
+    # save the results to file
+    with open("results.json", "w") as fp:
         json.dump(results, fp, indent=4)
+
 
 if __name__ == "__main__":
     main()
--- scripts/constants.py
+++ scripts/constants.py
@@ -1,61 +1,76 @@
 # optim_option values is from https://github.com/Trusted-AI/AIF360/blob/master/examples/demo_optim_data_preproc.ipynb
 
-from aif360.algorithms.preprocessing.optim_preproc_helpers.distortion_functions\
-            import get_distortion_adult, get_distortion_german, get_distortion_compas
-from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions\
-        import load_preproc_data_adult, load_preproc_data_german, load_preproc_data_compas
+from aif360.algorithms.preprocessing.optim_preproc_helpers.distortion_functions import (
+    get_distortion_adult,
+    get_distortion_german,
+    get_distortion_compas,
+)
+from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import (
+    load_preproc_data_adult,
+    load_preproc_data_german,
+    load_preproc_data_compas,
+)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 DATASETS = {
-    'german': {
-        'sensitive_attribute' : 'sex',     # if changed, apply changes to to the original_dataset attribute
-        'privileged_groups' : [{'sex': 1}],
-        'unprivileged_groups' : [{'sex': 0}],
-        'original_dataset':  load_preproc_data_german(['sex']),
-        'optim_options': {
+    "german": {
+        "sensitive_attribute": "sex",  # if changed, apply changes to to the original_dataset attribute
+        "privileged_groups": [{"sex": 1}],
+        "unprivileged_groups": [{"sex": 0}],
+        "original_dataset": load_preproc_data_german(["sex"]),
+        "optim_options": {
             "distortion_fun": get_distortion_german,
             "epsilon": 0.1,
             "clist": [0.99, 1.99, 2.99],
-            "dlist": [.1, 0.05, 0]
+            "dlist": [0.1, 0.05, 0],
         },
-
     },
-    'adult': {
-        'sensitive_attribute': 'sex',
-        'privileged_groups': [{'sex': 1}],
-        'unprivileged_groups': [{'sex': 0}],
-        'original_dataset': load_preproc_data_adult(['sex']),
-        'optim_options': {
+    "adult": {
+        "sensitive_attribute": "sex",
+        "privileged_groups": [{"sex": 1}],
+        "unprivileged_groups": [{"sex": 0}],
+        "original_dataset": load_preproc_data_adult(["sex"]),
+        "optim_options": {
             "distortion_fun": get_distortion_adult,
             "epsilon": 0.05,
             "clist": [0.99, 1.99, 2.99],
-            "dlist": [.1, 0.05, 0]
-        }
+            "dlist": [0.1, 0.05, 0],
+        },
     },
-    'compas': {
-        'sensitive_attribute': 'sex',
-        'privileged_groups': [{'sex': 1}],
-        'unprivileged_groups': [{'sex': 0}],
-        'original_dataset': load_preproc_data_compas(['sex']),
-        'optim_options': {
+    "compas": {
+        "sensitive_attribute": "sex",
+        "privileged_groups": [{"sex": 1}],
+        "unprivileged_groups": [{"sex": 0}],
+        "original_dataset": load_preproc_data_compas(["sex"]),
+        "optim_options": {
             "distortion_fun": get_distortion_compas,
             "epsilon": 0.05,
             "clist": [0.99, 1.99, 2.99],
-            "dlist": [.1, 0.05, 0]
-        }
-    }
+            "dlist": [0.1, 0.05, 0],
+        },
+    },
 }
 
 
 CLASSIFIERS = {
     "Logistic Regression": LogisticRegression(),
-    "Random Forest": RandomForestClassifier(max_depth=10, n_estimators=5, max_features=2)
+    "Random Forest": RandomForestClassifier(
+        max_depth=10, n_estimators=5, max_features=2
+    ),
 }
 
 HYPERPARAMETERS = {
     "Reweighing": [{}],
-    "DisparateImpactRemover": [{'threshold': 0.01}, {'threshold': 0.5}, {'threshold': 0.5}],
+    "DisparateImpactRemover": [
+        {"repair_level": 0.01},
+        {"repair_level": 0.5},
+        {"repair_level": 0.5},
+    ],
     "OptimPreproc": [{}],
-    "LFR": [{}],
+    "LFR": [
+        {"k": 5, "Ax": 0.01, "Ay": 1.0, "Az": 50.0, "threshold": 0.01},
+        {"k": 5, "Ax": 0.01, "Ay": 1.0, "Az": 50.0, "threshold": 0.5},
+        {"k": 5, "Ax": 0.01, "Ay": 1.0, "Az": 50.0, "threshold": 0.1},
+    ],
 }
