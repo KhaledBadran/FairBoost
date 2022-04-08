1d105467bf443f4e595cbc4ac97af2022cb4d2f6
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2022-04-08 15:10:56 -0400

    pushed fairboost grid

--- scripts/configs/constants.py
+++ scripts/configs/constants.py
@@ -8,15 +8,19 @@ from .enums import Dataset_name, Preproc_name
 SEEDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # ------------------------------- FAIRBOOST GRID ------------------------------- #
-
-
 FairBoost_param_grid = {
     'init': {"bootstrap_type": [
         Bootstrap_type.NONE,
         Bootstrap_type.DEFAULT,
         Bootstrap_type.CUSTOM,
     ]},
-    'preprocessing': [[Preproc_name.DisparateImpactRemover]]
+    'preprocessing': [[Preproc_name.OptimPreproc],
+                      [Preproc_name.LFR],
+                      [Preproc_name.Reweighing],
+                      [Preproc_name.OptimPreproc, Preproc_name.LFR],
+                      [Preproc_name.OptimPreproc, Preproc_name.Reweighing],
+                      [Preproc_name.LFR, Preproc_name.Reweighing],
+                      [Preproc_name.Reweighing, Preproc_name.LFR, Preproc_name.OptimPreproc]]
 }
 
 
