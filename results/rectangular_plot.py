aca8b119610098dd70122a7bbc97c7f10dbb3897
rached1997 <bouchoucharached@gmail.com>
2022-04-10 12:28:52 -0400

    modified stability plot

--- results/rectangular_plot.py
+++ results/rectangular_plot.py
@@ -10,6 +10,17 @@ from plotnine import *
 from plotnine.data import mpg
 from typeguard import typechecked
 
+def Merge(dict1, dict2):
+    """
+    Merges two dicts.
+            Parameters:
+                    dict1 : first dict to merge
+                    dict2 : second dict to merge
+            Returns:
+                    res (Dict): a dict merging dict1 and dict2
+    """
+    res = {**dict1, **dict2}
+    return res
 
 def read_data_baseline(path):
     """
@@ -34,7 +45,6 @@ def read_data_baseline(path):
                     dict[key] = classifier_value
     return dict
 
-
 def read_data_fairboost(path):
     """
     Generates a dict object representing the data in the fairboost_splits.json.
@@ -48,16 +58,19 @@ def read_data_fairboost(path):
         results = json.load(f)
     for dataset_key, dataset_value in results["results"].items():
         for preprocessing_key, preprocessing_value in dataset_value.items():
-            for i in range(len(preprocessing_value)):
-                for classifier_key, classifier_value in preprocessing_value[i]["results"].items():
-                    if preprocessing_key == "fairboost":
-                        key = dataset_key + "-" + preprocessing_key + "-" + classifier_key + "-" \
+            if preprocessing_key == "fairboost":
+                for i in range(6, len(preprocessing_value), 7):
+                    for classifier_key, classifier_value in preprocessing_value[i]["results"].items():
+                        key = "Fairboost : " + dataset_key + "-" + preprocessing_key + "-" + classifier_key + "-" \
                               + preprocessing_value[i]["hyperparameters"]["init"]['bootstrap_type']
                         dict[key] = classifier_value
-                    else:
-                        key = dataset_key + "-" + preprocessing_key + "-" + classifier_key + "-" \
-                              + preprocessing_value[i]["hyperparameters"]['bootstrap_type']
-                        dict[key] = classifier_value
+            else:
+                for i in range(len(preprocessing_value)):
+                    for classifier_key, classifier_value in preprocessing_value[i]["results"].items():
+                        if preprocessing_value[i]["hyperparameters"]['bootstrap_type'] == "NONE":
+                            key = "Fairboost : " + dataset_key + "-" + preprocessing_key + "-" + classifier_key + "-" \
+                                  + preprocessing_value[i]["hyperparameters"]['bootstrap_type']
+                            dict[key] = classifier_value
     return dict
 
 
@@ -108,8 +121,8 @@ def rectangular_plot(data: Dict, plots_path, dataset_name="", classifier_name=""
          + scale_y_continuous(name="fairness")
          # + scale_y_continuous(name="fairness", limits=(0,1.2))
          + geom_rect(data=dataframe, mapping=aes(xmin=dataframe["x1"], xmax=dataframe["x2"], ymin=dataframe["y1"],
-                                                 ymax=dataframe["y2"], fill=dataframe["t"]),alpha=1)
-                     # color="black", alpha=1)
+                                                 ymax=dataframe["y2"], fill=dataframe["t"]), color="black", alpha=0.6)
+         # color="black", alpha=1)
          # + geom_text(aes(x=dataframe["x1"] + (dataframe["x2"] - dataframe["x1"]) / 2,
          #                 y=dataframe["y1"] + (dataframe["y2"] - dataframe["y1"]) / 2, label=dataframe["r"]),
          #             data=dataframe, size=5)
@@ -117,22 +130,26 @@ def rectangular_plot(data: Dict, plots_path, dataset_name="", classifier_name=""
          + theme(legend_margin=-10, legend_box_spacing=0)
          )
     print(g)
-    g.save(plots_path+dataset_name+"-"+classifier_name+".png")
+    g.save(plots_path + dataset_name + "-" + classifier_name + ".png")
     return g
 
-def main():
-    data = read_data_baseline("json_files/baseline_splits.json")
-    plots_path = "plots/baseline/"
 
-    # data = read_data_fairboost("json_files/fairboost_splits.json")
+def main():
+    data_baseline = read_data_baseline("json_files/baseline_splits.json")
+    # plots_path = "plots/baseline/"
+
+    data_fairboost = read_data_fairboost("json_files/fairboost_splits.json")
     # plots_path = "plots/fairboost/"
 
+    data = Merge(data_baseline, data_fairboost)
+    plots_path = "plots/"
 
     datasets = ["german", "adult", "compas"]
     classifiers = ["Logistic Regression", "Random Forest"]
     for dataset in datasets:
         for classifier in classifiers:
-            rectangular_plot(data,plots_path = plots_path, dataset_name=dataset, classifier_name=classifier)
+            rectangular_plot(data, plots_path=plots_path, dataset_name=dataset, classifier_name=classifier)
+
 
 if __name__ == '__main__':
     main()
