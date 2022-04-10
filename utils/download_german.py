09fbab7c7e1790b69292d9fdf031c482428dac7b
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2022-04-10 19:12:20 -0400

    removed plot files and useless files in the repo

--- utils/download_german.py
+++
@@ -1,5 +0,0 @@
-import requests
-response = requests.get(
-    "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data")
-with open(f'../data/german.data', 'w') as f:
-    f.write(response.text)
