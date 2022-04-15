4d829d09c20d1c500b820dd8de1fc336b6d70b3d
KhaledBadran <badrankhaled97@gmail.com>
2022-04-15 19:39:18 -0400

    remove unused print statments
    - i removed print statments in Fairboost evaluation script

--- scripts/evaluation_fairboost.py
+++ scripts/evaluation_fairboost.py
@@ -56,7 +56,6 @@ def train_test_bagging_baseline(
     results = defaultdict(dict)
     pp = [wrappers.NoPreprocessing() for _ in range(3)]
     for clf_name, clf in CLASSIFIERS.items():
-        # print(f"\nFairboost classifier name: {clf_name}")
         ens = FairBoost(clf, pp, **hyperparameters)
         ens = ens.fit(train_dataset)
         y_pred = ens.predict(test_dataset)
@@ -184,7 +183,6 @@ def train_test_fairboost(
 
     preprocessed_datasets = None
     for clf_name, clf in CLASSIFIERS.items():
-        # print(f"\nFairboost classifier name: {clf_name}")
         try:
             # Training + prediction
             ens = FairBoost(clf, pp, **hyperparameters["init"])
