1711f112370dac2e9592ecc51599f477f64b626f
rached1997 <bouchoucharached@gmail.com>
2022-04-21 16:29:12 -0400

    Adding Fairboost plots

--- scripts/results/RQ1_harmonic_mean_analysis.py
+++ scripts/results/RQ1_harmonic_mean_analysis.py
@@ -227,6 +227,91 @@ def preprocess_dataframe(df, dataset: str, classifier: str, n_elem=5):
     return preprecessed_df
 
 
+@typechecked
+def select_configurations(df, dataset: str, classifier: str):
+    """
+    Selects and preprocesses the dataframe of the following configs :
+    baseline, RW, LFR, OP, Fairboost-None, Fairboost-Custom, Fairboost-Default.
+    :param df: dataframe to be preprocessed
+    :param dataset: the dataset name
+    :param classifier: the classifier name
+    :return: the preprocessed dataframe
+    """
+    # select the rows by the classifiers and datasets names
+    preprecessed_df = df.loc[(df["dataset"] == dataset) & (df["classifier"] == classifier)]
+
+    # select the top n_elems having the highest mean of the h_mean
+    preprecessed_df = preprecessed_df.loc[(preprecessed_df["experiment"] == "preprocessing")
+                                          | (preprecessed_df["experiment"] == "baseline")
+                                          | ((preprecessed_df["experiment"] == "fairboost")
+                                             & (preprecessed_df["preprocessing"] == "LFR,OptimPreproc,Reweighing")
+                                             )
+                                          ]
+
+    preprecessed_df['Mean'] = preprecessed_df["h_mean"].apply(np.median)
+
+    # Explode the h_mean list to rows
+    preprecessed_df = preprecessed_df.explode("h_mean")
+
+    preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("Reweighing", "RW")
+    preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("OptimPreproc", "OP")
+
+    # Add the column (1) experiment/(2) bootstrap_type/ (3)preprocessing which will be used as the x_axis
+    preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"] = \
+        "(1) " + preprecessed_df["experiment"].str.upper() + "\n" \
+        + "(2) " + preprecessed_df["bootstrap_type"].str.upper() + "\n" \
+        + "(3) " + preprecessed_df["preprocessing"].str.upper()
+
+    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
+                        ('BASELINE'), "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'BASELINE'
+
+    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
+                        ('PREPROCESSING & RW'),
+                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'RW'
+
+    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
+                        ('PREPROCESSING & LFR'),
+                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'LFR'
+
+    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
+                        ('PREPROCESSING & OP'),
+                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'OP'
+
+    return preprecessed_df
+
+
+@typechecked
+def select_fairboost_configs(df, dataset: str, classifier: str, n_elem=5):
+    """
+    Selects and preprocesses the dataframe of Fairboost configs.
+    :param df: dataframe to be preprocessed
+    :param dataset: the dataset name
+    :param classifier: the classifier name
+    :param n_elem: the number of configuration to analyse (e.g 5 means the top 5 configs)
+    :return: the preprocessed dataframe
+    """
+    # select the rows by the classifiers and datasets names
+    preprecessed_df = df.loc[
+        (df["experiment"] == "fairboost") & (df["dataset"] == dataset) & (df["classifier"] == classifier)]
+
+    # select the top n_elems having the highest mean of the h_mean
+    preprecessed_df['Mean'] = preprecessed_df["h_mean"].apply(np.median)
+    preprecessed_df = preprecessed_df.sort_values("Mean", ascending=False)[:n_elem]
+
+    # Explode the h_mean list to rows
+    preprecessed_df = preprecessed_df.explode("h_mean")
+
+    preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("Reweighing", "RW")
+    preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("OptimPreproc", "OP")
+
+    # Add the column (1) experiment/(2) bootstrap_type/ (3)preprocessing which will be used as the x_axis
+    preprecessed_df["(1) bootstrap_type / (2)preprocessing"] = \
+        "(1) " + preprecessed_df["bootstrap_type"].str.upper() + "\n" \
+        + "(2) " + preprecessed_df["preprocessing"].str.upper()
+
+    return preprecessed_df
+
+
 def plot_unique_boxplot(df):
     """
     this method will generate a plot for every combination of classifiers and datasets.
@@ -271,7 +356,15 @@ def plot_multiple_boxplots(df):
     frames = []
     for classifier in classifiers_list:
         for dataset in datasets_list:
-            plot_df = preprocess_dataframe(df=df, dataset=dataset, classifier=classifier, n_elem=5)
+            # this function plots the n top elements
+            # plot_df = preprocess_dataframe(df=df, dataset=dataset, classifier=classifier, n_elem=5)
+
+            # this function plots the baseline, RW, LFR, OP, Fairboost-None, Fairboost-Custom, Fairboost-Default
+            # plot_df = select_configurations(df=df, dataset=dataset, classifier=classifier)
+
+            # this function plots the configurations of Fairboost
+            plot_df = select_fairboost_configs(df=df, dataset=dataset, classifier=classifier)
+
             frames.append(plot_df)
 
     results_df = pd.concat(frames)
@@ -285,11 +378,12 @@ def plot_multiple_boxplots(df):
         'whiskerprops': {'color': 'black'},
         'capprops': {'color': 'black'}
     }
-    grid = sns.FacetGrid(results_df, col="classifier", row="dataset", height=4, aspect=2, sharey=True, sharex=False)
-    grid.map(sns.boxplot, "(1) experiment / (2) bootstrap_type / (3)preprocessing", "h_mean", linewidth=1, width=0.5,
+    grid = sns.FacetGrid(results_df, col="classifier", row="dataset", height=3, aspect=4, sharey=True, sharex=False)
+    grid.map(sns.boxplot, "(1) bootstrap_type / (2)preprocessing", "h_mean", linewidth=1, width=0.5,
              **PROPS)
 
-    plt.savefig("Boxplots/Combined_plots_classifier_as_rows_scaled_y.pdf")
+    # plt.savefig("Boxplots/Combined_plots_classifier_as_rows_scaled_y.pdf")
+    plt.savefig("Boxplots/fairboost.pdf")
 
 
 def main():
