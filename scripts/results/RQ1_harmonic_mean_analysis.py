61726a5ab1988d96be2f043c77562accb5fbd722
rached1997 <bouchoucharached@gmail.com>
2022-04-22 12:50:59 -0400

    Sort plots by the dataset name

--- scripts/results/RQ1_harmonic_mean_analysis.py
+++ scripts/results/RQ1_harmonic_mean_analysis.py
@@ -240,7 +240,7 @@ def select_configurations(df, dataset: str, classifier: str):
     # select the rows by the classifiers and datasets names
     preprecessed_df = df.loc[(df["dataset"] == dataset) & (df["classifier"] == classifier)]
 
-    # select the top n_elems having the highest mean of the h_mean
+    # select specific configs
     preprecessed_df = preprecessed_df.loc[(preprecessed_df["experiment"] == "preprocessing")
                                           | (preprecessed_df["experiment"] == "baseline")
                                           | ((preprecessed_df["experiment"] == "fairboost")
@@ -256,27 +256,44 @@ def select_configurations(df, dataset: str, classifier: str):
     preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("Reweighing", "RW")
     preprecessed_df["preprocessing"] = preprecessed_df["preprocessing"].str.replace("OptimPreproc", "OP")
 
-    # Add the column (1) experiment/(2) bootstrap_type/ (3)preprocessing which will be used as the x_axis
-    preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"] = \
-        "(1) " + preprecessed_df["experiment"].str.upper() + "\n" \
-        + "(2) " + preprecessed_df["bootstrap_type"].str.upper() + "\n" \
-        + "(3) " + preprecessed_df["preprocessing"].str.upper()
+    # Add the column Preprocessing type which will be used as the x_axis
+    preprecessed_df["Preprocessing type"] = \
+        preprecessed_df["experiment"].str.upper() + " : \n" \
+        + preprecessed_df["bootstrap_type"].str.upper() + "\n" \
+        + preprecessed_df["preprocessing"].str.upper()
 
-    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
-                        ('BASELINE'), "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'BASELINE'
+    # Change the names of some elements preprocessing types
+    "PREPROCESSING" + "\n" \
+    + " : " + preprecessed_df["bootstrap_type"].str.upper() + "\n" \
+    + "RW"
 
-    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
-                        ('PREPROCESSING & RW'),
-                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'RW'
+    RW = list("PREPROCESSING" + " : \n" + preprecessed_df["bootstrap_type"].str.upper() + "\n" + "RW")[0]
 
-    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
-                        ('PREPROCESSING & LFR'),
-                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'LFR'
+    LFR = list("PREPROCESSING" + " : \n" + preprecessed_df["bootstrap_type"].str.upper() + "\n" + "LFR")[0]
 
-    preprecessed_df.loc[preprecessed_df["(1) experiment / (2) bootstrap_type / (3)preprocessing"].str.contains
-                        ('PREPROCESSING & OP'),
-                        "(1) experiment / (2) bootstrap_type / (3)preprocessing"] = 'OP'
+    OP = list("PREPROCESSING" + " : \n" + preprecessed_df["bootstrap_type"].str.upper() + "\n" + "OP")[0]
 
+    preprecessed_df.loc[preprecessed_df["Preprocessing type"].str.contains(
+        'BASELINE'), "Preprocessing type"] = 'BASELINE'
+
+    preprecessed_df["Preprocessing type"] = \
+        np.where(preprecessed_df["Preprocessing type"] == RW,
+                 "Reweighing \n(RW)", preprecessed_df["Preprocessing type"])
+
+    preprecessed_df["Preprocessing type"] = \
+        np.where(preprecessed_df["Preprocessing type"] == LFR,
+                 "Learning Fair \n Representations (LFR)", preprecessed_df["Preprocessing type"])
+
+    preprecessed_df["Preprocessing type"] = \
+        np.where(preprecessed_df["Preprocessing type"] == OP,
+                 "Optimized \n Preprocessing \n (OP)", preprecessed_df["Preprocessing type"])
+
+    # Add the column Dataset - Classifier
+    preprecessed_df["Dataset - Classifier"] = \
+        preprecessed_df["dataset"].str.upper() \
+        + " - " + preprecessed_df["classifier"].str.upper()
+
+    preprecessed_df = preprecessed_df.drop(['dataset', 'classifier'], axis=1)
     return preprecessed_df
 
 
@@ -360,16 +377,36 @@ def plot_multiple_boxplots(df):
             # plot_df = preprocess_dataframe(df=df, dataset=dataset, classifier=classifier, n_elem=5)
 
             # this function plots the baseline, RW, LFR, OP, Fairboost-None, Fairboost-Custom, Fairboost-Default
-            # plot_df = select_configurations(df=df, dataset=dataset, classifier=classifier)
+            plot_df = select_configurations(df=df, dataset=dataset, classifier=classifier)
 
             # this function plots the configurations of Fairboost
-            plot_df = select_fairboost_configs(df=df, dataset=dataset, classifier=classifier)
+            # plot_df = select_fairboost_configs(df=df, dataset=dataset, classifier=classifier, n_elem=7)
 
             frames.append(plot_df)
 
     results_df = pd.concat(frames)
+
+    # The following lines sorts the data by the name of the classifier
+    data_classifier = ["GERMAN - LOGISTIC REGRESSION", "GERMAN - RANDOM FOREST",
+                       "ADULT - LOGISTIC REGRESSION", "ADULT - RANDOM FOREST",
+                       "COMPAS - LOGISTIC REGRESSION", "COMPAS - RANDOM FOREST"]
+
+    preprocessing_type = ["BASELINE", "Reweighing \n(RW)", "Learning Fair \n Representations (LFR)",
+                          "Optimized \n Preprocessing \n (OP)","FAIRBOOST : \nNONE\nLFR,OP,RW",
+                          "FAIRBOOST : \nDEFAULT\nLFR,OP,RW","FAIRBOOST : \nCUSTOM\nLFR,OP,RW"]
+
+    results_df["Dataset - Classifier"] = results_df["Dataset - Classifier"].astype("category")
+    results_df["Dataset - Classifier"].cat.set_categories(data_classifier, inplace=True)
+
+    results_df["Preprocessing type"] = results_df["Preprocessing type"].astype("category")
+    results_df["Preprocessing type"].cat.set_categories(preprocessing_type, inplace=True)
+
+    results_df = results_df.sort_values(["Dataset - Classifier", "Preprocessing type"])
+
+    # We drop the useless columns
     results_df = results_df.drop(['experiment', 'bootstrap_type', 'preprocessing', 'Mean'], axis=1)
 
+    # We plot the boxplot
     fig, ax = plt.subplots(figsize=(50, 50))
 
     PROPS = {
@@ -378,12 +415,13 @@ def plot_multiple_boxplots(df):
         'whiskerprops': {'color': 'black'},
         'capprops': {'color': 'black'}
     }
-    grid = sns.FacetGrid(results_df, col="classifier", row="dataset", height=3, aspect=4, sharey=True, sharex=False)
-    grid.map(sns.boxplot, "(1) bootstrap_type / (2)preprocessing", "h_mean", linewidth=1, width=0.5,
+    # grid = sns.FacetGrid(results_df, col="classifier", row="dataset", height=3, aspect=4, sharey=True, sharex=False)
+    grid = sns.FacetGrid(results_df, row="Dataset - Classifier", height=3, aspect=4, sharey=True, sharex=True)
+    grid.map(sns.boxplot, "Preprocessing type", "h_mean", linewidth=1, width=0.5,
              **PROPS)
 
-    # plt.savefig("Boxplots/Combined_plots_classifier_as_rows_scaled_y.pdf")
-    plt.savefig("Boxplots/fairboost.pdf")
+    plt.savefig("Boxplots/Combined_plots_classifier_as_rows_scaled_y_v2.pdf")
+    # plt.savefig("Boxplots/fairboost.pdf")
 
 
 def main():
