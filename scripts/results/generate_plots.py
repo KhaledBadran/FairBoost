4cb90da058614b83e67e4b8343558c6d4017f0f7
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2022-04-25 19:49:11 -0400

    added script to generate all plots

---
+++ scripts/results/generate_plots.py
@@ -0,0 +1,10 @@
+import box_plots_rq1, rectangular_plot, paretto_plots
+
+
+def main():
+    box_plots_rq1.main()
+    rectangular_plot.main()
+    paretto_plots.main()
+
+if __name__ == '__main__':
+    main()
